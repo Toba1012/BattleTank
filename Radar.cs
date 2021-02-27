e5807c7e0fb80a07fc9a2c7f9bed150ac75ee89c
鳥羽祥平 <tobashohei@tobashouheinoMacBook-Air.local>
2021-02-27 18:53:52 +0900

    敵の感知機能

---
+++ Radar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar: MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transform.root.LookAt(target);
+        }
+    }
+}
