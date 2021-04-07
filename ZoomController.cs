9253a2565f06d71a89904a40deed54eff68c73e8
Toba1012 <marxdm03@gmail.com>
2021-04-07 23:09:48 +0900

    照準のズーム機能

---
+++ ZoomController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomController : MonoBehaviour
+{
+    private Camera cam;
+    private float zoom;
+    private float view;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        view = cam.fieldOfView;
+    }
+
+    void Update()
+    {
+        cam.fieldOfView = view + zoom;
+
+        if (cam.fieldOfView < 20f)
+        {
+            cam.fieldOfView = 20f;
+        }
+
+        if (cam.fieldOfView > 60f)
+        {
+            cam.fieldOfView = 60f;
+        }
+
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            zoom -= 1.2f;
+
+            if (zoom < -40f)
+            {
+                zoom = -40f;
+            }
+        }
+        else
+        {
+            zoom += 1.2f;
+
+            if (zoom > 0)
+            {
+                zoom = 0;
+            }
+        }
+
+        print("zoomの値" + zoom);
+    }
+}
